14712da8bff472ad6e4959c2ee7e55186967dffb
HrishiDarade <daradeh2002@gmail.com>
2025-08-29 12:30:31 +0530

    updated

--- src/org/hrishi/utils/DockerUtils.groovy
+++ src/org/hrishi/utils/DockerUtils.groovy
@@ -3,19 +3,18 @@ package org.hrishi.utils
 class DockerUtils {
     
     //static build method 
-    static void buildImage(def script, String project, String tag) {
-        script.echo "Building the Docker Image: ${project}:${tag}"
-        script.sh "docker build -t ${project}:${tag} ."
+    static void buildImage(def script, String project, String imagetag) {
+        script.echo "Building the Docker Image: ${project}:${imagetag}"
+        script.sh "docker build -t ${project}:${imagetag} ."
     }
     //static push method
-    static void pushImage (def script, String project, String tag, String credId = 'dockerCred') {
+    static void pushImage (def script, String project, String imagetag, String credId = 'dockerCred') {
         script.withCredentials([script.usernamePassword(credentialsId: credId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-            script.sh """
-            set -xe
+            script.sh '''#!/bin/bash -xe
             docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
-            docker image tag ${project}:${tag} ${DOCKER_USER}/${project}:${tag}
-            docker push ${DOCKER_USER}/${project}:${tag}
-        """
+            docker image tag ${project}:${imagetag} ${DOCKER_USER}/${project}:${imagetag}
+            docker push ${DOCKER_USER}/${project}:${imagetag}
+            '''
         }
     }
 
