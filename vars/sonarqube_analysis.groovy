e3eeddf9975817bd017984251eabcaa7626a1d9f
Hrishikesh Darade <99548417+HrishiDarade@users.noreply.github.com>
2025-10-03 15:03:57 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,8 +1,6 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
     withSonarQubeEnv("${SonarQubeAPI}"){
         sh """
-        cd spring-boot-server
-        mvn clean install -DskipTests
         ${SONAR_HOME}/bin/sonar-scanner \
             -Dsonar.projectKey=${ProjectKey} \
             -Dsonar.projectName=${Projectname} \
